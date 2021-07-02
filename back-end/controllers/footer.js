effc7c52e61fe82208ab9aebe9f83ba506511db1
jantizio <emanuele0246@gmail.com>
2021-07-02 13:53:50 +0200

    cambiato estensione nei controller

--- back-end/controllers/footer.js
+++ back-end/controllers/footer.js
@@ -1,32 +1,37 @@
 import fs from "fs";
 
-const
-	pagina="footer",
-	imgDir="./img/"+pagina+"/",
-	erroreFile="Si è verificato un errore interno al server: il file richiesto potrebbe non esistere",
-	erroreParam="Non sono stati inseriti correttamente i parametri",
-	imgExtension=".png";
+const pagina = "footer",
+  imgDir = "./img/" + pagina + "/",
+  erroreFile =
+    "Si è verificato un errore interno al server: il file richiesto potrebbe non esistere",
+  erroreParam = "Non sono stati inseriti correttamente i parametri",
+  imgExtensions = [".png", ".jpg"];
 
-export const Responder=(req,res)=>
-{
-	const {fileName}=req.params;
-	if(fileName=="img") Img(req,res);
-	else res.writeHead(400).end(erroreParam);
-}
+export const Responder = (req, res) => {
+  const { fileName } = req.params;
+  if (fileName == "img") Img(req, res);
+  else res.writeHead(400).end(erroreParam);
+};
 
-const Img=(req,res)=>
-{
-	const {img}=req.query;
-	console.log("Immagine "+pagina+": "+img);
-	const path=imgDir+img+imgExtension;
-	fs.readFile
-	(
-		path,
-		(err,content)=>
-		{
-			console.log(err);
-			if(err) res.writeHead(404).end(erroreFile);
-			else res.writeHead(200,{"Content-type":"image/jpg"}).end(content);
-		}
-	);
-}
+const Img = (req, res) => {
+  const { img } = req.query;
+  console.log("Immagine " + pagina + ": " + img);
+  //const path = imgDir + img + imgExtension;
+
+  let path = imgDir + img;
+  let contentType = "image/";
+  imgExtensions.every((imgExtension) => {
+    let tempPath = imgDir + img + imgExtension;
+    if (fs.existsSync(tempPath)) {
+      path += imgExtension;
+      contentType += imgExtension.substring(1);
+      return false;
+    }
+    return true;
+  });
+
+  fs.readFile(path, (err, content) => {
+    if (err) res.writeHead(404).end(erroreFile);
+    else res.writeHead(200, { "Content-type": contentType }).end(content);
+  });
+};
--- back-end/controllers/header.js
+++ back-end/controllers/header.js
@@ -1,64 +1,46 @@
 import fs from "fs";
 
-const
-	pagina="header",
-	imgDir="./img/"+pagina+"/",
-	txtDir="./texts/"+pagina+"/",
-    erroreFile="Si è verificato un errore interno al server: il file richiesto potrebbe non esistere",
-    erroreParam="Non sono stati inseriti correttamente i parametri",
-	imgExtension=".png",
-	txtExtension=".js";
+const pagina = "header",
+  imgDir = "./img/" + pagina + "/",
+  txtDir = "./texts/" + pagina + "/",
+  erroreFile =
+    "Si è verificato un errore interno al server: il file richiesto potrebbe non esistere",
+  erroreParam = "Non sono stati inseriti correttamente i parametri",
+  imgExtension = ".png",
+  txtExtension = ".js";
 
-export const Responder=(req,res)=>
-{
-	const {fileName}=req.params;
-	if(fileName=="img") Img(req,res);
-	else if(fileName=="js") Txt(req,res);
-	else res.writeHead(400).end(erroreParam);
+export const Responder = (req, res) => {
+  const { fileName } = req.params;
+  if (fileName == "img") Img(req, res);
+  else if (fileName == "js") Txt(req, res);
+  else res.writeHead(400).end(erroreParam);
 };
-const Txt=(req,res)=>
-{
-	const path=txtDir+"1"+txtExtension;
-	fs.readFile
-	(
-		path,
-		(err,content)=>
-		{
-			if(err) res.writeHead(404).end(erroreFile);
-			else res.writeHead(200,{"Content-type":"application/javascript"}).end(content);
-		}
-	)
-}
-const Img=(req,res)=>
-{
-	const {img}=req.query;
-	if(img==="bandiera")
-	{
-		const {bandiera}=req.query;
-		console.log("Immagine "+pagina+" della bandiera: "+bandiera);
-		const path=imgDir+bandiera+imgExtension;
-		fs.readFile
-		(
-			path,
-			(err,content)=>
-			{
-				if(err) res.writeHead(404).end(erroreFile);
-				else res.writeHead(200,{"Content-type":"image/jpg"}).end(content);
-			}
-		);
-	}
-	else if(img==="logo")
-	{
-		console.log("Immagine "+pagina+" del "+img);
-		const path=imgDir+img+imgExtension;
-		fs.readFile
-		(
-			path,
-			(err,content)=>
-			{
-				if(err) res.writeHead(404).end(erroreFile);
-				else res.writeHead(200,{"Content-type":"image/jpg"}).end(content);
-			}
-		);
-	}
-}
+const Txt = (req, res) => {
+  const path = txtDir + "1" + txtExtension;
+  fs.readFile(path, (err, content) => {
+    if (err) res.writeHead(404).end(erroreFile);
+    else
+      res
+        .writeHead(200, { "Content-type": "application/javascript" })
+        .end(content);
+  });
+};
+const Img = (req, res) => {
+  const { img } = req.query;
+  if (img === "bandiera") {
+    const { bandiera } = req.query;
+    console.log("Immagine " + pagina + " della bandiera: " + bandiera);
+    const path = imgDir + bandiera + imgExtension;
+    fs.readFile(path, (err, content) => {
+      if (err) res.writeHead(404).end(erroreFile);
+      else res.writeHead(200, { "Content-type": "image/jpg" }).end(content);
+    });
+  } else if (img === "logo") {
+    console.log("Immagine " + pagina + " del " + img);
+    const path = imgDir + img + imgExtension;
+    fs.readFile(path, (err, content) => {
+      if (err) res.writeHead(404).end(erroreFile);
+      else res.writeHead(200, { "Content-type": "image/jpg" }).end(content);
+    });
+  }
+};
